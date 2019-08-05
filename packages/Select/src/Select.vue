81c48e4ba3490da3d02f467acdae56969fca132e
guoshan <540449517@qq.com>
2019-08-05 18:16:38 +0800

    新增helloword文件/修改配置

--- packages/Select/src/Select.vue
+++ packages/Select/src/Select.vue
@@ -1,12 +1,20 @@
 <template>
-  <el-select v-model="value" size="small">
-    <el-option v-for="item in options" :key="item.prop" :value="item.prop" :label="item.label"/>
+  <el-select
+    v-model="value"
+    size="small"
+  >
+    <el-option
+      v-for="item in options"
+      :key="item.prop"
+      :value="item.prop"
+      :label="item.label"
+    />
   </el-select>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-    props: {
+  props: {
     options: {
       type: Array,
       default() {
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import Select from '../lib/Select.js'
+import Select from '../packages/Select/index'
 
 window.Vue = Vue
 
